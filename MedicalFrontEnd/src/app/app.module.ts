af0a1d3e114ae5925c56fe3780077d15ff71badd
Sergio Ramirez <47802275+chechoXR@users.noreply.github.com>
2020-11-24 22:54:56 -0500

    Toolbar y botones principales

---
+++ MedicalFrontEnd/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
