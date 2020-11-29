80e32284f107295f5b592d015e92d1303f60348a
Sergio Ramirez <47802275+chechoXR@users.noreply.github.com>
2020-11-28 20:07:35 -0500

    Se pueden crear pacientes

--- MedicalFrontEnd/src/app/agregar-paciente/agregar-paciente.component.ts
+++ MedicalFrontEnd/src/app/agregar-paciente/agregar-paciente.component.ts
@@ -1,8 +1,10 @@
+
 import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { MatDialog } from '@angular/material/dialog';
 
 export interface paciente{
   id:number,
@@ -31,20 +33,18 @@ export interface eps{
   styleUrls: ['./agregar-paciente.component.sass']
 })
 export class AgregarPacienteComponent implements AfterViewInit {
-  nombre = new FormControl('', [Validators.required]);
-  fecha = new FormControl('',[Validators.required]);
-  tipoID = new FormControl('',[Validators.required]);
-  id = new FormControl('',[Validators.required]);
-  epsval = new FormControl('',[Validators.required]);
-  historia = new FormControl('',[Validators.required]);
+  
 
+  tipoSel;
+  epsSel;
+  errorVisible = false;
   tiposID=[];
   eps = [];
 
   private url = 'https://medical-backend-web-android.herokuapp.com';
   
 
-  constructor(private router:Router, private http:HttpClient) {}
+  constructor(private router:Router, private http:HttpClient, private dialog:MatDialog) {}
 
   ngAfterViewInit(): void {
     this.cargarTiposID();
@@ -58,7 +58,7 @@ export class AgregarPacienteComponent implements AfterViewInit {
     this.tiposID = [];
     result.subscribe((res)=>{
       res.forEach(element => {
-        this.tiposID.push(element.tipoIdentificacion);   
+        this.tiposID.push(element);   
       });
     });
     return true;
@@ -70,35 +70,50 @@ export class AgregarPacienteComponent implements AfterViewInit {
     this.eps = [];
     result.subscribe((res)=>{
       res.forEach(element => {
-        this.eps.push(element.eps);   
+        this.eps.push(element);   
       });
     });
     return true;
   }
 
-  guardar(){
+  async guardar(){
     var nombre = document.getElementById("nombre") as HTMLInputElement;
     var fechaNacimiento = document.getElementById("fecha") as HTMLInputElement;
     var tipoID = document.getElementById("tipoID") as HTMLSelectElement;
     var id = document.getElementById("id") as HTMLInputElement;
     var eps = document.getElementById("eps") as HTMLSelectElement;
     var historia = document.getElementById("historia") as HTMLInputElement;
-
+/*
     console.log(nombre.value);
-    console.log(fechaNacimiento);
-    console.log(tipoID.options);
+    console.log(fechaNacimiento.value);
+    console.log(this.tipoSel);
     console.log(id.value);
-    console.log(eps.selectedOptions);
+    console.log(this.epsSel);
     console.log(historia.value);
+  */  
+    if(nombre.value != "" && fechaNacimiento.value != "" && this.tipoSel != undefined && id.value !="" && this.epsSel != undefined && historia.value != "") {
+      const fec = new Date(fechaNacimiento.value);
+      //console.log("fec "+fec.toISOString());
 
+      
+      const result = this.http.post(this.url+"/web/pacientes",{
+        "nombre": nombre.value,
+        "fechaNacimiento": fec.toISOString(),
+        "identificacion": id.value,
+        "tipoIdentificacion": this.tipoSel,
+        "eps":this.epsSel,
+        "historiaClinica":historia.value
+      });
 
-    /*
-    const result = this.http.post(this.url+"/web/pacientes",{
+      result.subscribe((res)=>{
+        this.router.navigateByUrl("");
+      });
+      
+      
+    }else{
+      this.errorVisible = true;
+    }
 
-    });
-    
-    this.router.navigateByUrl("");
-*/
   }
 
 
